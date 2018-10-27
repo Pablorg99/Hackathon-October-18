91825d764764e762ac93b23c8af5f7392a4dbe03
i82nafea <i82nafea@uco.es>
2018-10-27 13:24:40 +0200

    Comando start añadido

--- bot.py
+++ bot.py
@@ -5,3 +5,8 @@ from libs import private as token
 bot = telebot.TeleBot(token)
 sp = spotipy.Spotify()
 
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+	sendMessage(message.chat.id ,'Bienvenido a SBotify')
+
+bot.polling()
