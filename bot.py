d06ac2bf8de5a205a70848069cc6943fb406cd16
Pablorg99 <i72rogup@uco.es>
2018-10-27 15:48:25 +0200

    bot.py updated

--- bot.py
+++ bot.py
@@ -1,12 +1,20 @@
 import spotipy
+import spotipy.util as util
 import telebot
 from libs import private as token
+from libs import credentials as cred
+from libs import functions as func
 
 bot = telebot.TeleBot(token)
 sp = spotipy.Spotify()
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['log_in'])
 def send_welcome(message):
-	sendMessage(message.chat.id ,'Bienvenido a SBotify')
+	cid = message.chat.id
+	bot.send_message(cid ,'Bienvenido a SBotify')
+	bot.send_message(cid, 'Dime tu nombre de usuario')
+	#username = func.catch_username(message)
+
+#util.prompt_for_user_token(username, 'playlist-modify-private', )
 
 bot.polling()
