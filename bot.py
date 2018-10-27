53dcdb3acc445cb871184f894c42679b868fd9ca
Pablorg99 <i72rogup@uco.es>
2018-10-27 20:12:37 +0200

    bot.py modified

--- bot.py
+++ bot.py
@@ -1,20 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import spotipy
 import spotipy.util as util
 import telebot
-from libs import private as token
+from flask import request
+from libs import private as TOKEN
 from libs import credentials as cred
-from libs import functions as func
 
-bot = telebot.TeleBot(token)
-sp = spotipy.Spotify()
+bot = telebot.TeleBot(TOKEN.token)
 
 @bot.message_handler(commands=['log_in'])
-def send_welcome(message):
+def logIn(message):
 	cid = message.chat.id
+	scope = 'playlist-modify-private'
 	bot.send_message(cid ,'Bienvenido a SBotify')
-	bot.send_message(cid, 'Dime tu nombre de usuario')
-	#username = func.catch_username(message)
-
-#util.prompt_for_user_token(username, 'playlist-modify-private', )
+	bot.send_message(cid, 'Dime tu nombre de usuario de Spotify')
+	@bot.message_handler(func = lambda message: True)
+	def getUsername(message):
+		username = str(message)
+		tk = util.prompt_for_user_token(username, scope, cred.client_id, cred.client_secret, cred.redirect_uri)
+		sp = spotipy.Spotify(auth = tk)
 
 bot.polling()
--- libs/credentials.py
+++ libs/credentials.py
@@ -1,2 +1,3 @@
-CLIENT_ID = 'PRUEBA'
-CLIENT_SECRET = 'PRUEBA2'
+client_id = 'd84ccadca5e64e4ca0ed34f3cb172f8f'
+client_secret = '83ea3ace553746299c240149b33091fa'
+redirect_uri = 'http://localhost:8888/callback'
