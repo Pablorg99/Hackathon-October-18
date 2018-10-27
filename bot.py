853ad2b791ccdb3fd64a0955ff505a7fbf5e35ce
AlexTheMagnus <i72lulem@uco.es>
2018-10-27 20:06:03 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -4,8 +4,3 @@ from libs import private as token
 
 bot = telebot.TeleBot(token)
 sp = spotipy.Spotify()
-
-
-@bot.message_handler(commands=['setplaylist'])
-def setplaylist(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
